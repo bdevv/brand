bd6ebfd44691d9d4b903f8affab75197591d58a9
bdevv <bdv.villan@gmail.com>
2024-09-27 08:45:44 -0500

    Updated again

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,7 @@
-export const ROBOT_ID = "6c6f2244-4168-4e68-a348-337c5bffaf65";
-export const ROBOT1_ID = "6794b7e3-8246-4b95-b019-1cd31b3d2fe2";
-export const ROBOT2_ID = "d60d4b86-167f-4f33-b294-d2db1394b477";
+export const ROBOT_ID = "";
+export const ROBOT1_ID = "";
+export const ROBOT2_ID = "";
 export const ORIGIN_URL =
   "https://www.espressozone.com/espresso-machines/semi-automatic-espresso-machines";
 export const BROWSE_AI_TOKEN =
-  "Bearer 5eed59e0-7d9e-4e72-a010-1a1910d3b5c9:95376b31-7226-4e1d-884c-6f01369528b0";
+  "Bearer ";
